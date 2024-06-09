b268a31f35cd994c85c3bf79d8d46571c1e4e563
kishan <guptakishan492@gmail.com>
2024-06-09 21:36:04 +0530

    bug

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,13 +1,13 @@
 import axios from  'axios'
 
-const server = "https://ecommerce-backend-th3l.onrender.com/"
+const server = "https://ecommerce-backend-th3l.onrender.com"
 const localhost = "http://localhost:8000"
 export const API_BASE_URL = server
 
 const jwt = localStorage.getItem("jwt")
 
 export const api = axios.create({
-    baseURL: API_BASE_URL,
+    baseURL: API_BASE_URL, 
     // headers: {
     //     "Authorization": `Bearer ${jwt}`,
     //     "Content-Type": "application/json"
