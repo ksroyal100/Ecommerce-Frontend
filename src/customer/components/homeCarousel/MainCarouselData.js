06abd64efd3ecf1109d8fbda2567fb5deb71fe26
kishan <guptakishan492@gmail.com>
2026-01-04 16:01:02 +0530

    some components are updated

--- src/customer/components/homeCarousel/MainCarouselData.js
+++ src/customer/components/homeCarousel/MainCarouselData.js
@@ -1,18 +1,14 @@
 export const mainCarouselData = [
     {
-        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/7/17_-_copy-1.jpg",
+        image: "https://www.ethnicplus.in/cdn/shop/files/2_e289c019-a7e5-4ae1-be8c-f43da939d14a.jpg",
         path: "/women/clothing/lengha_choli"
     },
     {
-        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/4/14-1.jpg",
+        image: "https://www.ethnicplus.in/cdn/shop/files/12_d8d322d4-26af-4947-a251-cde51c420eca.jpg",
         path: "/women/clothing/lengha_choli"
     },
     {
-        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/5/15-.jpg",
-        path: "/women/clothing/lengha_choli"
-    },
-    {
-        image: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/6/16-11.jpg",
+        image: "https://www.ethnicplus.in/cdn/shop/files/13_07ff9d23-8efe-4ff1-955c-1ed8f2d36758.jpg",
         path: "/women/clothing/lengha_choli"
     },
 ]
